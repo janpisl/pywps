2896d7670ed2f38470eab54790ed2e822c42583d
janpisl <janpisl@hotmail.com>
2018-06-19 17:31:30 +0200

    Add test for file storage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,7 +3,8 @@ import atexit
 import shutil
 import tempfile
 from pywps import FORMATS
-from pywps.inout.storage import DummyStorage
+from pywps.inout.storage import DummyStorage, STORE_TYPE
+from pywps.inout.storage.file import FileStorage
 from pywps import ComplexOutput
 import os
 
@@ -45,3 +46,31 @@ class DummyStorageTestCase(unittest.TestCase):
         vector_output.file = get_vector_file()
         assert not self.storage.store("some data")
 
+
+class FileStorageTestCase(unittest.TestCase):
+    """FileStorage tests
+    """
+
+    def setUp(self):
+        global TEMP_DIRS
+        tmp_dir = tempfile.mkdtemp()
+        TEMP_DIRS.append(tmp_dir)
+
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        pass
+
+    def test_file_storage(self):
+        assert isinstance(self.storage, FileStorage)
+
+
+    def test_store(self):
+        vector_output = ComplexOutput('vector', 'Vector output',
+                             supported_formats=[FORMATS.GML])
+        vector_output.file = get_vector_file()
+
+        self.assertEqual(len(self.storage.store(vector_output)), 3) 
+        self.assertEqual(self.storage.store(vector_output)[0], STORE_TYPE.PATH)
+        self.assertIsInstance(self.storage.store(vector_output)[1], str)
+        self.assertIsInstance(self.storage.store(vector_output)[2], str)
