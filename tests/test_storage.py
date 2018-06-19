16afbea95f1fea31dd3ac9e7a30d63b5d309ad12
janpisl <janpisl@hotmail.com>
2018-06-19 20:43:40 +0200

    Update PgStorageTestCase

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -6,8 +6,10 @@ from pywps import FORMATS
 from pywps.inout.storage import DummyStorage, STORE_TYPE
 from pywps.inout.storage.file import FileStorage
 from pywps.inout.storage.db.pg import PgStorage
+from pywps.inout.storage.db import DbStorage
 from pywps import ComplexOutput
 import os
+from pywps import configuration as config
 
 TEMP_DIRS=[]
 
@@ -22,6 +24,19 @@ atexit.register(clear)
 def get_vector_file():
 
     return os.path.join(os.path.dirname(__file__), "data", "gml", "point.gml")
+
+
+def get_connstr():
+
+    dbsettings = "db"
+    target = "dbname={} user={} password={} host={} port={}".format(
+        config.get_config_value(dbsettings, "dbname"),
+        config.get_config_value(dbsettings, "user"),
+        config.get_config_value(dbsettings, "password"),
+        config.get_config_value(dbsettings, "host"),
+        config.get_config_value(dbsettings, "port")
+    )
+    return target
 
 class DummyStorageTestCase(unittest.TestCase):
     """Storage test case
@@ -87,6 +102,10 @@ class PgStorageTestCase(unittest.TestCase):
         TEMP_DIRS.append(tmp_dir)
 
         self.storage = PgStorage()
+        self.storage.target = "dbname=pisl user=pisl password=password host=localhost port=5432"
+        #this does not work
+        #self.storage.target = get_connstr()
+        self.storage.schema_name = "testovaci_schemaadssf"
 
     def tearDown(self):
         pass
@@ -95,11 +114,16 @@ class PgStorageTestCase(unittest.TestCase):
         assert isinstance(self.storage, PgStorage)
 
 
-    def test_store(self):
+    def test_vector_store(self):
         vector_output = ComplexOutput('vector', 'Vector output',
                              supported_formats=[FORMATS.GML])
         vector_output.file = get_vector_file()
         vector_output.output_format = FORMATS.GML
 
-
+        self.assertEqual(len(self.storage.store(vector_output)), 3) 
         self.assertEqual(self.storage.store(vector_output)[0], STORE_TYPE.DB)
+        self.assertIsInstance(self.storage.store(vector_output)[1], str)
+        self.assertIsInstance(self.storage.store(vector_output)[2], str)
+
+    #TODO: test raster store, test other store
+    # sqlite
