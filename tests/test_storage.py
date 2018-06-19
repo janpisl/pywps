1c36dd1ee3b1c61d819b92f5a195792f488b1193
janpisl <janpisl@hotmail.com>
2018-06-19 14:52:42 +0200

    Introduce test_storage
    Introduce test_storage and implement dummy storage test

---
+++ tests/test_storage.py
@@ -0,0 +1,48 @@
+import unittest
+import atexit
+import shutil
+import tempfile
+from pywps import Format
+from pywps.inout.storage import DummyStorage
+from pywps import ComplexOutput
+import os
+
+TEMP_DIRS=[]
+
+def clear():
+    """Delete temporary files
+    """
+    for d in TEMP_DIRS:
+        shutil.rmtree(d)
+
+atexit.register(clear)
+
+def get_vector_file():
+
+    return os.path.join(os.path.dirname(__file__), "data", "gml", "point.gml")
+
+class DummyStorageTestCase(unittest.TestCase):
+    """Storage test case
+    """
+
+    def setUp(self):
+        global TEMP_DIRS
+        tmp_dir = tempfile.mkdtemp()
+        TEMP_DIRS.append(tmp_dir)
+
+        self.storage = DummyStorage()
+
+    def tearDown(self):
+        pass
+
+    def test_dummy_storage(self):
+        assert isinstance(self.storage, DummyStorage)
+
+
+    def test_store(self):
+        vector_output = ComplexOutput('vector', 'Vector output',
+                             supported_formats=[
+                                Format('application/gml+xml')
+                            ])
+        vector_output.file = get_vector_file()
+        assert not self.storage.store("some data")
